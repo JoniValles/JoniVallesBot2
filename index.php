f10ac85606b4855fc6265c3c6556f545ff24a4e4
Joni <jonivalles@gmail.com>
2018-06-30 13:47:12 +0200

    Intercambios

--- index.php
+++ index.php
@@ -938,7 +938,7 @@ $conn->set_charset("utf8");
  $finalData = implode(" ", $data);
  $nombre = "a";
  
- $query = "insert into intercambios (Nombre,Ofrece) values ($nombre,'$trainer[1]')";
+ $query = "insert into intercambios (Nombre,Ofrece) values ('$nombre','$trainer[1]')";
  //executing the query 
  mysqli_query($conn, $query) or die('Error querying database.');
 //$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
